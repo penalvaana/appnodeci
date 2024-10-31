9199997fb3fc5272c286d456d43aec0851535fc7
penalvaana2 <ana.penalva91@gmail.com>
2024-10-31 13:51:17 +0000

    add script and port

--- evaluacion/app.js
+++ evaluacion/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-const path = require('path'); // Importar el módulo path
+const path = require('path'); 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Middleware para servir archivos estáticos
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Rutas
 const books = require('./routes/books');
 const authors = require('./routes/authors');
 app.use('/books', books);
 app.use('/authors', authors);
 
-// Manejo de rutas incorrectas
 app.use('/*', (req, res) => {
     res.status(404).json({
         message: 'Incorrect route or params',
